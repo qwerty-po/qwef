b91aa9ab00efebfe29686a51b76605e7825ead70
qwerty-po <qwerty@postech.ac.kr>
2024-01-14 17:46:36 +0900

    refactor: simplify peb using nt.typedvar

--- qwef.py
+++ qwef.py
@@ -1024,306 +1024,8 @@ class TEB():
         except:
             return None
 class PEB():
-    @dataclass
-    class PEBInfo():
-        InheritedAddressSpace: int
-        ReadImageFileExecOptions: int
-        BeingDebugged: int
-        
-        BitField: int # contain ImageUsesLargePages to IsLongPathAwareProcess
-        ImageUsesLargePages: int
-        IsProtectedProcess: int
-        IsImageDynamicallyRelocated: int
-        SkipPatchingUser32Forwarders: int
-        IsPackagedProcess: int
-        IsAppContainer: int
-        IsProtectedProcessLight: int
-        IsLongPathAwareProcess: int
-        
-        Mutant: int
-        ImageBaseAddress: int
-        Ldr: int
-        ProcessParameters: int
-        SubSystemData: int
-        ProcessHeap: int
-        FastPebLock: int
-        AtlThunkSListPtr: int
-        IFEOKey: int
-        
-        CrossProcessFlags: int # contain ProcessInJob to ReservedBits0
-        ProcessInJob: int
-        ProcessInitializing: int
-        ProcessUsingVEH: int
-        ProcessUsingVCH: int
-        ProcessUsingFTH: int
-        ProcessPreviouslyThrottled: int
-        ProcessCurrentlyThrottled: int
-        ProcessImagesHotPatched: int
-        ReservedBits0: int
-        
-        KernelCallbackTable: int # union with UserSharedInfoPtr
-        UserSharedInfoPtr: int
-        
-        SystemReserved: int
-        AtlThunkSListPtr32: int
-        ApiSetMap: int
-        TlsExpansionCounter: int
-        TlsBitmap: int
-        TlsBitmapBits: typing.List[int]
-        ReadOnlySharedMemoryBase: int
-        SharedData: int
-        ReadOnlyStaticServerData: int
-        AnsiCodePageData: int
-        OemCodePageData: int
-        UnicodeCaseTableData: int
-        NumberOfProcessors: int
-        NtGlobalFlag: int
-        CriticalSectionTimeout: int
-        HeapSegmentReserve: int
-        HeapSegmentCommit: int
-        HeapDeCommitTotalFreeThreshold: int
-        HeapDeCommitFreeBlockThreshold: int
-        NumberOfHeaps: int
-        MaximumNumberOfHeaps: int
-        ProcessHeaps: int
-        GdiSharedHandleTable: int
-        ProcessStarterHelper: int
-        GdiDCAttributeList: typing.List[int]
-        LoaderLock: int
-        OSMajorVersion: int
-        OSMinorVersion: int
-        OSBuildNumber: int
-        OSCSDVersion: int
-        OSPlatformId: int
-        ImageSubsystem: int
-        ImageSubsystemMajorVersion: int
-        ImageSubsystemMinorVersion: int
-        ActiveProcessAffinityMask: int
-        GdiHandleBuffer: int
-        PostProcessInitRoutine: int
-        TlsExpansionBitmap: int
-        TlsExpansionBitmapBits: typing.List[int]
-        
-        SessionId: int
-        AppCompatFlags: int
-        AppCompatFlagsUser: int
-        pShimData: int
-        AppCompatInfo: int
-        CSDVersion: typing.List[int] # Length, Maxlength, string ptr
-        
-        ActivationContextData: int
-        ProcessAssemblyStorageMap: int
-        SystemDefaultActivationContextData: int
-        SystemAssemblyStorageMap: int
-        MinimumStackCommit: int
-        SparePointers: typing.List[int]
-        
-        PatchLoaderData: int
-        ChpeV2ProcessInfo: int
-        AppModelFeatureState: int
-        SpareUlongs: typing.List[int]
-        
-        ActiveCodePage: int
-        OemCodePage: int
-        UseCaseMapping: int
-        UnusedNlsField: int
-        WerRegistrationData: int
-        WerShipAssertPtr: int
-        Spare: int
-        pImageHeaderHash: int
-        
-        TracingFlags: int # contain HeapTracingEnabled to SpareTracingBits
-        HeapTracingEnabled: int
-        CritSecTracingEnabled: int
-        LibLoaderTracingEnabled: int
-        SpareTracingBits: int
-        
-        CsrServerReadOnlySharedMemoryBase: int
-        TppWorkerpListLock: int
-        TppWorkerpList: ListEntry
-        
-        WaitOnAddressHashTable: typing.List[int]
-        
-        TelemetryCoverageHeader: int
-        CloudFileFlags: int
-        CloudFileDiagFlags: int
-        PlaceholderCompatibilityMode: int
-        PlaceholderCompatibilityModeReserved: typing.List[int]
-        
-        LeapSecondData: int # contain LeapSecondFlags to Reserved
-        LeapSecondFlags: int
-        SixtySecondEnabled: int
-        Reserved: int
-        
-        NtGlobalFlag2: int
-        ExtendedFeatureDisableMask: int
-        
-        def __init__(self, peb_address):
-            if context.arch == pykd.CPUType.I386:
-                bytes_data: bytes = memoryaccess.get_bytes(peb_address, 0x484)
-                
-                self.InheritedAddressSpace = int.from_bytes(bytes_data[0x000:0x001], byteorder="little")
-                self.ReadImageFileExecOptions = int.from_bytes(bytes_data[0x001:0x002], byteorder="little")
-                self.BeingDebugged = int.from_bytes(bytes_data[0x002:0x003], byteorder="little")
-                
-                self.BitField = int.from_bytes(bytes_data[0x003:0x004], byteorder="little")
-                self.ImageUsesLargePages = int.from_bytes(bytes_data[0x003:0x004], byteorder="little") & 0x1
-                self.IsProtectedProcess = (int.from_bytes(bytes_data[0x003:0x004], byteorder="little") >> 1) & 0x1
-                self.IsImageDynamicallyRelocated = (int.from_bytes(bytes_data[0x003:0x004], byteorder="little") >> 2) & 0x1
-                self.SkipPatchingUser32Forwarders = (int.from_bytes(bytes_data[0x003:0x004], byteorder="little") >> 3) & 0x1
-                self.IsPackagedProcess = (int.from_bytes(bytes_data[0x003:0x004], byteorder="little") >> 4) & 0x1
-                self.IsAppContainer = (int.from_bytes(bytes_data[0x003:0x004], byteorder="little") >> 5) & 0x1
-                self.IsProtectedProcessLight = (int.from_bytes(bytes_data[0x003:0x004], byteorder="little") >> 6) & 0x1
-                self.IsLongPathAwareProcess = (int.from_bytes(bytes_data[0x003:0x004], byteorder="little") >> 7) & 0x1
-                
-                self.Mutant = int.from_bytes(bytes_data[0x004:0x008], byteorder="little")
-                self.ImageBaseAddress = int.from_bytes(bytes_data[0x008:0x00c], byteorder="little")
-                self.Ldr = int.from_bytes(bytes_data[0x00c:0x010], byteorder="little")
-                self.ProcessParameters = int.from_bytes(bytes_data[0x010:0x0014], byteorder="little")
-                self.SubSystemData = int.from_bytes(bytes_data[0x014:0x018], byteorder="little")
-                self.ProcessHeap = int.from_bytes(bytes_data[0x018:0x01c], byteorder="little")
-                self.FastPebLock = int.from_bytes(bytes_data[0x01c:0x020], byteorder="little")
-                self.AtlThunkSListPtr = int.from_bytes(bytes_data[0x020:0x024], byteorder="little")
-                self.IFEOKey = int.from_bytes(bytes_data[0x024:0x028], byteorder="little")
-                self.CrossProcessFlags = int.from_bytes(bytes_data[0x028:0x02c], byteorder="little")
-                self.ProcessInJob = (int.from_bytes(bytes_data[0x028:0x02c], byteorder="little")) & 0x1
-                self.ProcessInitializing = (int.from_bytes(bytes_data[0x028:0x02c], byteorder="little") >> 1) & 0x1
-                self.ProcessUsingVEH = (int.from_bytes(bytes_data[0x028:0x02c], byteorder="little") >> 2) & 0x1
-                self.ProcessUsingVCH = (int.from_bytes(bytes_data[0x028:0x02c], byteorder="little") >> 3) & 0x1
-                self.ProcessUsingFTH = (int.from_bytes(bytes_data[0x028:0x02c], byteorder="little") >> 4) & 0x1
-                self.ProcessPreviouslyThrottled = (int.from_bytes(bytes_data[0x028:0x02c], byteorder="little") >> 5) & 0x1
-                self.ProcessCurrentlyThrottled = (int.from_bytes(bytes_data[0x028:0x02c], byteorder="little") >> 6) & 0x1
-                self.ProcessImagesHotPatched = (int.from_bytes(bytes_data[0x028:0x02c], byteorder="little") >> 7) & 0x1
-                self.ReservedBits0 = (int.from_bytes(bytes_data[0x028:0x02c], byteorder="little") >> 8) & 0xffffff
-                
-                self.KernelCallbackTable = int.from_bytes(bytes_data[0x02c:0x030], byteorder="little")
-                self.UserSharedInfoPtr = int.from_bytes(bytes_data[0x02c:0x030], byteorder="little")
-                
-                self.SystemReserved = int.from_bytes(bytes_data[0x030:0x034], byteorder="little")
-                self.AtlThunkSListPtr32 = int.from_bytes(bytes_data[0x034:0x038], byteorder="little")
-                self.ApiSetMap = int.from_bytes(bytes_data[0x038:0x03c], byteorder="little")
-                self.TlsExpansionCounter = int.from_bytes(bytes_data[0x03c:0x040], byteorder="little")
-                self.TlsBitmap = int.from_bytes(bytes_data[0x040:0x044], byteorder="little")
-                
-                self.TlsBitmapBits = []
-                for i in range(0x044, 0x04c, 0x4):
-                    self.TlsBitmapBits.append(int.from_bytes(bytes_data[i:i+4], byteorder="little"))
-                
-                self.ReadOnlySharedMemoryBase = int.from_bytes(bytes_data[0x04c:0x050], byteorder="little")
-                self.SharedData = int.from_bytes(bytes_data[0x050:0x054], byteorder="little")
-                self.ReadOnlyStaticServerData = int.from_bytes(bytes_data[0x054:0x058], byteorder="little")
-                self.AnsiCodePageData = int.from_bytes(bytes_data[0x058:0x05c], byteorder="little")
-                self.OemCodePageData = int.from_bytes(bytes_data[0x05c:0x060], byteorder="little")
-                self.UnicodeCaseTableData = int.from_bytes(bytes_data[0x060:0x064], byteorder="little")
-                self.NumberOfProcessors = int.from_bytes(bytes_data[0x064:0x068], byteorder="little")
-                self.NtGlobalFlag = int.from_bytes(bytes_data[0x068:0x070], byteorder="little")
-                self.CriticalSectionTimeout = int.from_bytes(bytes_data[0x070:0x078], byteorder="little")
-                self.HeapSegmentReserve = int.from_bytes(bytes_data[0x078:0x07c], byteorder="little")
-                
-                self.HeapSegmentCommit = int.from_bytes(bytes_data[0x07c:0x080], byteorder="little")
-                self.HeapDeCommitTotalFreeThreshold = int.from_bytes(bytes_data[0x080:0x084], byteorder="little")
-                self.HeapDeCommitFreeBlockThreshold = int.from_bytes(bytes_data[0x084:0x088], byteorder="little")
-                self.NumberOfHeaps = int.from_bytes(bytes_data[0x088:0x08c], byteorder="little")
-                self.MaximumNumberOfHeaps = int.from_bytes(bytes_data[0x08c:0x090], byteorder="little")
-                self.ProcessHeaps = int.from_bytes(bytes_data[0x090:0x094], byteorder="little")
-                self.GdiSharedHandleTable = int.from_bytes(bytes_data[0x094:0x098], byteorder="little")
-                self.ProcessStarterHelper = int.from_bytes(bytes_data[0x098:0x09c], byteorder="little")
-                self.GdiDCAttributeList = int.from_bytes(bytes_data[0x09c:0x0a0], byteorder="little")
-                self.LoaderLock = int.from_bytes(bytes_data[0x0a0:0x0a4], byteorder="little")
-                self.OSMajorVersion = int.from_bytes(bytes_data[0x0a4:0x0a8], byteorder="little")
-                self.OSMinorVersion = int.from_bytes(bytes_data[0x0a8:0x0ac], byteorder="little")
-                self.OSBuildNumber = int.from_bytes(bytes_data[0x0ac:0x0ae], byteorder="little")
-                self.OSCSDVersion = int.from_bytes(bytes_data[0x0ae:0x0b0], byteorder="little")
-                self.OSPlatformId = int.from_bytes(bytes_data[0x0b0:0x0b4], byteorder="little")
-                self.ImageSubsystem = int.from_bytes(bytes_data[0x0b4:0x0b8], byteorder="little")
-                self.ImageSubsystemMajorVersion = int.from_bytes(bytes_data[0x0b8:0x0bc], byteorder="little")
-                self.ImageSubsystemMinorVersion = int.from_bytes(bytes_data[0x0bc:0x0c0], byteorder="little")
-                self.ActiveProcessAffinityMask = int.from_bytes(bytes_data[0x0c0:0x0c4], byteorder="little")
-                
-                self.GdiHandleBuffer = []
-                for i in range(0x0c4, 0x14c, 0x4):
-                    self.GdiHandleBuffer.append(int.from_bytes(bytes_data[i:i+4], byteorder="little"))
-                
-                self.PostProcessInitRoutine = int.from_bytes(bytes_data[0x14c:0x150], byteorder="little")
-                self.TlsExpansionBitmap = int.from_bytes(bytes_data[0x150:0x154], byteorder="little")
-                
-                self.TlsExpansionBitmapBits = []
-                for i in range(0x154, 0x1d4, 0x4):
-                    self.TlsExpansionBitmapBits.append(int.from_bytes(bytes_data[i:i+4], byteorder="little"))
-                    
-                self.SessionId = int.from_bytes(bytes_data[0x1d4:0x1d8], byteorder="little")
-                
-                self.AppCompatFlags = int.from_bytes(bytes_data[0x1d8:0x1e0], byteorder="little")
-                self.AppCompatFlagsUser = int.from_bytes(bytes_data[0x1e0:0x1e8], byteorder="little")
-                
-                self.pShimData = int.from_bytes(bytes_data[0x1e8:0x1ec], byteorder="little")
-                self.AppCompatInfo = int.from_bytes(bytes_data[0x1ec:0x1f0], byteorder="little")
-                
-                self.CSDVersion = [ 
-                                    int.from_bytes(bytes_data[0x1f0:0x1f2], byteorder="little"), \
-                                    int.from_bytes(bytes_data[0x1f2:0x1f4], byteorder="little"), \
-                                    int.from_bytes(bytes_data[0x1f4:0x1f8], byteorder="little")
-                                  ]
-                
-                self.ActivationContextData = int.from_bytes(bytes_data[0x1f8:0x1fc], byteorder="little")
-                self.ProcessAssemblyStorageMap = int.from_bytes(bytes_data[0x1fc:0x200], byteorder="little")
-                self.SystemDefaultActivationContextData = int.from_bytes(bytes_data[0x200:0x204], byteorder="little")
-                self.SystemAssemblyStorageMap = int.from_bytes(bytes_data[0x204:0x208], byteorder="little")
-                self.MinimumStackCommit = int.from_bytes(bytes_data[0x208:0x20c], byteorder="little")
-                
-                self.SparePointers = []
-                for i in range(0x20c, 0x214, 0x4):
-                    self.SparePointers.append(int.from_bytes(bytes_data[i:i+4], byteorder="little"))
-
-                self.PatchLoaderData = int.from_bytes(bytes_data[0x214:0x218], byteorder="little")
-                self.ChpeV2ProcessInfo = int.from_bytes(bytes_data[0x218:0x21c], byteorder="little")
-                self.AppModelFeatureState = int.from_bytes(bytes_data[0x21c:0x220], byteorder="little")
-                
-                self.SpareUlongs = []
-                for i in range(0x220, 0x228, 0x4):
-                    self.SpareUlongs.append(int.from_bytes(bytes_data[i:i+4], byteorder="little"))
-                    
-                self.ActiveCodePage = int.from_bytes(bytes_data[0x228:0x22a], byteorder="little")
-                self.OemCodePage = int.from_bytes(bytes_data[0x22a:0x22c], byteorder="little")
-                self.UseCaseMapping = int.from_bytes(bytes_data[0x22c:0x22e], byteorder="little")
-                self.UnusedNlsField = int.from_bytes(bytes_data[0x22e:0x230], byteorder="little")
-                self.WerRegistrationData = int.from_bytes(bytes_data[0x230:0x234], byteorder="little")
-                self.WerShipAssertPtr = int.from_bytes(bytes_data[0x234:0x238], byteorder="little")
-                self.Spare = int.from_bytes(bytes_data[0x238:0x23c], byteorder="little")
-                self.pImageHeaderHash = int.from_bytes(bytes_data[0x23c:0x240], byteorder="little")
-                
-                self.TracingFlags = int.from_bytes(bytes_data[0x240:0x248], byteorder="little")
-                self.HeapTracingEnabled = (int.from_bytes(bytes_data[0x240:0x248], byteorder="little")) & 0x1
-                self.CritSecTracingEnabled = (int.from_bytes(bytes_data[0x240:0x248], byteorder="little") >> 1) & 0x1
-                self.LibLoaderTracingEnabled = (int.from_bytes(bytes_data[0x240:0x248], byteorder="little") >> 2) & 0x1
-                self.SpareTracingBits = (int.from_bytes(bytes_data[0x240:0x248], byteorder="little") >> 3)
-                
-                self.CsrServerReadOnlySharedMemoryBase = int.from_bytes(bytes_data[0x248:0x250], byteorder="little")
-                self.TppWorkerpListLock = int.from_bytes(bytes_data[0x250:0x254], byteorder="little")
-                self.TppWorkerpList = ListEntry(int.from_bytes(bytes_data[0x254:0x258], byteorder="little"), int.from_bytes(bytes_data[0x258:0x25c], byteorder="little"))
-                
-                self.WaitOnAddressHashTable = []
-                for i in range(0x25c, 0x45c, 0x4):
-                    self.WaitOnAddressHashTable.append(int.from_bytes(bytes_data[i:i+4], byteorder="little"))
-                
-                self.TelemetryCoverageHeader = int.from_bytes(bytes_data[0x45c:0x460], byteorder="little")
-                self.CloudFileFlags = int.from_bytes(bytes_data[0x460:0x464], byteorder="little")
-                self.CloudFileDiagFlags = int.from_bytes(bytes_data[0x464:0x468], byteorder="little")
-                self.PlaceholderCompatibilityMode = int.from_bytes(bytes_data[0x468:0x469], byteorder="little")
-                self.PlaceholderCompatibilityModeReserved = int.from_bytes(bytes_data[0x469:0x470], byteorder="little")
-                self.LeapSecondData = int.from_bytes(bytes_data[0x470:0x474], byteorder="little")
-                
-                self.LeapSecondFlags = int.from_bytes(bytes_data[0x474:0x478], byteorder="little")
-                self.SixtySecondEnabled = (int.from_bytes(bytes_data[0x474:0x478], byteorder="little")) & 0x1
-                self.Reserved = (int.from_bytes(bytes_data[0x474:0x478], byteorder="little") >> 1)
-                
-                self.NtGlobalFlag2 = int.from_bytes(bytes_data[0x478:0x480], byteorder="little")
-                self.ExtendedFeatureDisableMask = int.from_bytes(bytes_data[0x480:0x484], byteorder="little")
-        
     def __init__(self):
-        self.peb: self.PEBInfo
-        
-        self.getPEBInfo()
+        self.peb = self.getPEBInfo()
     
     def getPEBAddress(self) -> typing.Union[int, None]:
         try:
@@ -1334,10 +1036,10 @@ class PEB():
         except:
             return None
     
-    def getPEBInfo(self) -> None:
-        self.peb: int = self.PEBInfo(self.getPEBAddress())
+    def getPEBInfo(self):
+        peb = nt.typedVar("_PEB", self.getPEBAddress())
         
-        return self.peb
+        return peb
         
 
 class SEHInfo():
@@ -1409,7 +1111,7 @@ class Heap(PEB):
         pass
 
     def get_heaps_address(self) -> typing.List[int]:
-        peb: PEB.PEBInfo = self.getPEBInfo()
+        peb = self.getPEBInfo()
         self.heaps = []
         
         for i in range(peb.NumberOfHeaps):
@@ -1423,6 +1125,7 @@ cmd: CmdManager = CmdManager()
 memoryaccess: MemoryAccess = MemoryAccess()
 colour: ColourManager = ColourManager()
 context: ContextManager = ContextManager()    
+nt = pykd.module("ntdll")
 
 vmmap: Vmmap = Vmmap()
 search: SearchPattern = SearchPattern()
